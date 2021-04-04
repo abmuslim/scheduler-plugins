4229b55fa44d23c26d2d56b0c3e98da5fa8ff301
Julio Renner <julio.renner@sap.com>
2021-04-04 19:48:17 -0300

    fix: build

--- pkg/networkmetrics/network_traffic.go
+++ pkg/networkmetrics/network_traffic.go
@@ -14,13 +14,26 @@ type NetworkTraffic struct {
 	handle framework.FrameworkHandle
 }
 
+// NetworkTrafficName is the name of the plugin used in the Registry and configurations.
+const NetworkTrafficName = "NodeNetworkTrafficScorer"
+
 var _ = framework.ScorePlugin(&NetworkTraffic{})
+
+// Name returns name of the plugin. It is used in logs, etc.
+func (n *NetworkTraffic) Name() string {
+	return NetworkTrafficName
+}
 
 func (n *NetworkTraffic) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
 	nodeInfo, err := n.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
 	if err != nil {
 		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("getting node %q from Snapshot: %v", nodeName, err))
 	}
+	fmt.Print(nodeInfo)
 
 	return 0, nil
+}
+
+func (n *NetworkTraffic) ScoreExtensions() framework.ScoreExtensions {
+	return nil
 }
